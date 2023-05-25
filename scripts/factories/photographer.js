df72c8ceef11d87da335a8aaae7764d495c5ecc4
Dylan Vicente <dvicente@fidme.com>
2023-05-25 15:41:54 +0200

    feat: add js & css homepage

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,74 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
-
+    const { name, portrait, id, city, country, tagline, price } = data;
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
+        const article = document.createElement('article');
+        article.className = 'photographer-article';
+
+        const link = document.createElement('a');
+        link.href = `photographer.html?photographer=${id}`
+        link.ariaLabel = name;
+        link.setAttribute("onkeydown", "openlink(event)");
+
+        const img = getUserPictureDOM();
+
+        const h2 = document.createElement('h2');
         h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+
+        const location = document.createElement('p');
+        location.textContent = `${city}, ${country}`;
+
+        const tag = document.createElement('p');
+        tag.textContent = tagline;
+
+        const prices = document.createElement('p');
+        prices.textContent = `${price}€/jour`;
+        
+        
+        article.appendChild( link);
+        link.appendChild( img );
+        link.appendChild( h2 );
+        article.appendChild( location );
+        article.appendChild( tag );
+        article.appendChild( prices );
+
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+
+    function getUserPictureDOM() {
+        const img = document.createElement('img');
+        img.setAttribute('src', picture);
+        img.setAttribute('alt', name);
+        img.setAttribute('loading', 'lazy');
+        img.className = 'user'
+
+        return (img);
+    }
+
+    function getUserProfilDOM() {
+        const profil = document.createElement('div');
+        profil.className = 'photographer-profile';
+
+        const title = document.createElement('h1');
+        title.textContent = name;
+
+        const location = document.createElement('h2');
+        location.textContent = `${city}, ${country}`;
+
+        const tag = document.createElement('p');
+        tag.textContent = tagline;
+
+        profil.appendChild(title);
+        profil.appendChild(location);
+        profil.appendChild(tag);
+
+        return (profil);
+    }
+
+
+
+
+
+    return { getUserPictureDOM, getUserProfilDOM, getUserCardDOM  }
 }
